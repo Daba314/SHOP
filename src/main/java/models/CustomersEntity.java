25b341aaaedbf897e3753c3dbf11ab3ebad11de5
Daba Dashiev <dabakrytoe@yandex.ru>
2020-10-02 21:39:39 -0400

    start

---
+++ src/main/java/models/CustomersEntity.java
@@ -0,0 +1,89 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers", schema = "public", catalog = "OnlineShop")
+public class CustomersEntity {
+    private int customerid;
+    private String firstname;
+    private String lastname;
+    private Long phonenumber;
+    private String address;
+
+    @Id
+    @Column(name = "customerid")
+    public int getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(int customerid) {
+        this.customerid = customerid;
+    }
+
+    @Basic
+    @Column(name = "firstname")
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Basic
+    @Column(name = "lastname")
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Basic
+    @Column(name = "phonenumber")
+    public Long getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(Long phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    @Basic
+    @Column(name = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CustomersEntity that = (CustomersEntity) o;
+
+        if (customerid != that.customerid) return false;
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+        if (phonenumber != null ? !phonenumber.equals(that.phonenumber) : that.phonenumber != null) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = customerid;
+        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (phonenumber != null ? phonenumber.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/models/OrdersEntity.java
@@ -0,0 +1,72 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders", schema = "public", catalog = "OnlineShop")
+public class OrdersEntity {
+    private int orderid;
+    private String status;
+    private String destination;
+
+    @Id
+    @Column(name = "orderid")
+    public int getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(int orderid) {
+        this.orderid = orderid;
+    }
+
+    @Basic
+    @Column(name = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "destination")
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrdersEntity that = (OrdersEntity) o;
+
+        if (orderid != that.orderid) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (destination != null ? !destination.equals(that.destination) : that.destination != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = orderid;
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (destination != null ? destination.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "OrdersEntity{" +
+                "orderid=" + orderid +
+                ", status='" + status + '\'' +
+                ", destination='" + destination + '\'' +
+                '}';
+    }
+}
